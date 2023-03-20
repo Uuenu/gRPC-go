7939698543524d48c9f39090f11ab839614eb0c5
Uuenu <nelsonff918@gmail.com>
2023-03-20 19:45:48 +0100

    feat serializer

---
+++ serializer/file_test.go
@@ -0,0 +1,27 @@
+package serializer
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"main.go/pb"
+	"main.go/sample"
+)
+
+func TestFileSerializer(t *testing.T) {
+	t.Parallel()
+
+	binaryFile := "../tmp/laptop.bin"
+
+	laptop1 := sample.NewLaptop()
+
+	err := WriteProtobufToBinaryFile(laptop1, binaryFile)
+	require.NoError(t, err)
+
+	laptop2 := &pb.Laptop{}
+
+	err2 := ReadProtobufFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err2)
+	require.True(t, proto.Equal(laptop1, laptop2))
+}
